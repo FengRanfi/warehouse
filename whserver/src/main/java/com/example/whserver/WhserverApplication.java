e007337b66e2bfeed5f9fe0b8790660f885fd557
fRanfi <3426626940@qq.com>
2025-02-19 20:16:14 +0800

    feat:初始化项目

---
+++ whserver/src/main/java/com/example/whserver/WhserverApplication.java
@@ -0,0 +1,13 @@
+package com.example.whserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhserverApplication.class, args);
+	}
+
+}
